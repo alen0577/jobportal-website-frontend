e379b209b3a25a3c0005d23c2c5160e78cce201e
Alen Antony <alenantony32@gmail.com>
2023-05-17 22:28:26 +0530

    modified

--- jobapp/views.py
+++ jobapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def loginhome(request):
+    return render(request,'loginhome.html')
+
 def index(request):
     return render(request,'index.html')
